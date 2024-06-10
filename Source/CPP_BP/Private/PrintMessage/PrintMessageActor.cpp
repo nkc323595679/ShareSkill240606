1296e78ead55dbe0d733d4c23c92b6bb04c436ee
nkc323595679 <nkc20236176@st.denpa.jp>
2024-06-10 10:09:13 +0900

    UE_LOG de nihongo dasu Visual studio build error hassei

--- Source/CPP_BP/Private/PrintMessage/PrintMessageActor.cpp
+++ Source/CPP_BP/Private/PrintMessage/PrintMessageActor.cpp
@@ -17,32 +17,17 @@ void APrintMessageActor::BeginPlay()
 	Super::BeginPlay();
 
 	
-	//if (GEngine)
-	//{
-	//	//ViewportのScreenに文字列を出力する
-	//	GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Cyan, TEXT("Screen Message"));
-	//}
-
-	// OutputLogにLogを出力する
-	//UE_LOG(LogTemp, Warning, TEXT("Log Message"));
-
-    // 1Byte文字（Asciiと同じ）
-	/*fstring messageen = "english";
-	fstring messageentext = text("english");
-
-	ue_log(logtemp, warning, text("english"));
-	ue_log(logtemp, warning, text("%s"), *messageen);
-	ue_log(logtemp, warning, text("%s"), *messageentext);*/
-
-	// 2Byte文字（文字コードごとに違う）
-	//FString MessageJP = "日本語";               // UTF-8(Textファイルの文字コードに依存)
-    //FString MessageJpText = TEXT("日本語");     // UTF-16
-
-	//UE_LOG(LogTemp, Warning, TEXT("日本語"));
-	//UE_LOG(LogTemp, Warning, TEXT("%s"), *MessageJP);
-	//UE_LOG(LogTemp, Warning, TEXT("%s"), *MessageJpText);
 	
 
+	//// 2Byte文字（文字コードごとに違う）
+	////FString MessageJP = "日本語";               // UTF-8(Textファイルの文字コードに依存)
+ //   FString MessageJpText = TEXT("日本語");     // UTF-16
+
+	//UE_LOG(LogTemp, Warning, TEXT("日本語"));
+	////UE_LOG(LogTemp, Warning, TEXT("%s"), *MessageJP);
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), *MessageJpText);
+	//
+	
 
 
 
