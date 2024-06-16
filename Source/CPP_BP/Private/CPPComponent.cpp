ac06126859cbcb5300428d0ab2c78811cd191685
nkc323595679 <nkc20236176@st.denpa.jp>
2024-06-16 16:34:37 +0900

    CPP_BP Page 29 End Add component in C++

---
+++ Source/CPP_BP/Private/CPPComponent.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPPComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+
+ACPPComponent::ACPPComponent()
+{
+	// SceneComponentを作成する
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+
+	// SceneComponentをRootComponentに設定する
+	RootComponent = DefaultSceneRoot;
+
+	// StaticMeshComponentを作成する
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+
+	// StaticMeshをLoadしてStaticMeshComponentのStaticMeshに設定する
+	UStaticMesh* Mesh = LoadObject<UStaticMesh>(NULL, TEXT("/Game/Egg/Meshes/SM_SampleCUbe"), NULL, LOAD_None, NULL);
+	StaticMesh->SetStaticMesh(Mesh);
+
+	// StaticMeshComponentをRootComponentにAttachする
+	StaticMesh->SetupAttachment(RootComponent);
+
+	// ArrowComponentを作成する
+	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComponent"));
+
+	// ArrowComponentの位置を設定する
+	Arrow->SetRelativeLocation(FVector(50.0f, 0.0f, 0.0f));
+
+	// ArrowComponentをStaticMeshComponentにAttachする
+	Arrow->SetupAttachment(StaticMesh);
+
+
+
+	// PointLightComponentを作成する
+	PointLight = CreateDefaultSubobject<UPointLightComponent>(TEXT("PointLightComponent"));
+
+	// PointLightComponentの位置を設定する
+	PointLight->SetRelativeLocation(FVector(130.0f, 0.0f, 0.0f));
+
+	// PointLightComponentをStaticMeshComponentにAttachする
+	PointLight->SetupAttachment(StaticMesh);
+
+
+}
+
+
+
+// Called when the game starts or when spawned
+void ACPPComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 変数を作成する
+	FString Message = "C++ Hello World!";
+
+	// PrintStringノードと同じ処理
+	// UKismetSystemLibraryクラスのPrintString関数を呼び出す
+	UKismetSystemLibrary::PrintString(
+		this
+		, Message
+		, true
+		, true
+		, TextColor// Textのカラー情報に変数TextColorを設定
+		, Duration
+		, TEXT("None"));
+}
+
