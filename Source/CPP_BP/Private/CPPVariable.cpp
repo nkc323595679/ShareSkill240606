87db44eb355ec5119ae0c7cf52365302b52507b2
nkc323595679 <nkc20236176@st.denpa.jp>
2024-06-16 11:47:03 +0900

    CPP_BP Chapter 27 end

---
+++ Source/CPP_BP/Private/CPPVariable.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPPVariable.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetStringLibrary.h" // 追加
+
+// Called when the game starts or when spawned
+void ACPPVariable::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// 変数を作成する
+	FString Message = "C++ Hello World!";
+
+	
+
+	// PrintStringノードと同じ処理
+	// UKismetSystemLibraryクラスのPrintString関数を呼び出す
+	UKismetSystemLibrary::PrintString(
+		this
+		, Message
+		, true
+		, true
+		, TextColor // Textのカラー情報に変数TextColorを設定
+		, Duration
+		, TEXT("None"));
+}
